a8469d936c89d7a646d05d16dab88418fca2f8bc
Nulenka <117688741+Nulenka@users.noreply.github.com>
2023-03-05 18:00:11 +0300

    Last version of phone book

---
+++ data_create.py
@@ -0,0 +1,14 @@
+def name_data():
+    name = input('Enter the name: ')
+    print('Nice name!')
+    return name
+
+
+def surname_data():
+    surname = input('ENter the surname: ')
+    return surname
+
+
+def phone_data():
+    phone = input('Enter the phone number: ')
+    return phone
