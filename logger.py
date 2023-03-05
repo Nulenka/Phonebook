d35d3d5f97a1ffb951e84dde29490bc9c39287dc
Nulenka <117688741+Nulenka@users.noreply.github.com>
2023-03-05 19:16:44 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,4 +1,4 @@
-from Phonebook/blob/main/data_create.py import name_data, surname_data, phone_data, address_data
+from data_create import name_data, surname_data, phone_data, address_data
 
 def input_data():
     name = name_data()
